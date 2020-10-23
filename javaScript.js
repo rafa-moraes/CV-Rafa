5576a5ab736d13c290cd394547e007ab5b8493e7
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2020-10-23 19:20:41 +0100

    Update

--- javaScript.js
+++ javaScript.js
@@ -1,12 +1,25 @@
+
 function responsive() {
-    let navbar = document.querySelector(".navbar");
+    let navbar = document.querySelector("#my-navbar");    
     if (navbar.className === "navbar"){
-        navbar.className += "responsive";
+        navbar.className += " responsive";
     } else {
         navbar.className = "navbar"
     }
+}
 
-
+function close() {
+    let navbar = document.querySelector("#my-navbar");
+    if (navbar.className === ".responsive"){
+        navbar.className = "navbar";
+    }
 
 
 }
+
+
+
+
+
+
+
